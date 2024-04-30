34f3c06cbc32910d49dcc48a2505704c60dc747d
MohidulCreative <90836840+MhidulCreative@users.noreply.github.com>
2024-04-30 20:16:58 +0530

    some code formate and cors packeage install

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
-import express from 'express';
-import { APP_PORT, DB_URL } from './config';
+import express from "express";
+import { APP_PORT, DB_URL } from "./config";
 const app = express();
-import routes from './routers'
+import routes from "./routers";
 import errorHandler from "./middlewares/erorHandler";
 const mongoose = require("mongoose");
-import path from 'path';
+import path from "path";
+import cors from "cors";
 
 global.appRoot = path.resolve(__dirname);
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json())
-app.use('/api', routes)
-app.use('/uploads', express.static('uploads'))
- 
+app.use(express.json());
+app.use("/api", routes);
+app.use("/uploads", express.static("uploads"));
+// app.use(
+//     cors({
+//         origin: "http://localhost:5000",
+//     })
+// );
+
 // Database conncetion
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect(DB_URL);
+    await mongoose.connect(DB_URL);
     console.log("database connected");
 }
 
-
-app.use(errorHandler)
+app.use(errorHandler);
 app.listen(APP_PORT, () => {
     console.log(`listening on port ${APP_PORT}.`);
-})
+});
